b1cca247880c9f918d5a937cdfb5231394f66cda
ivannissimrch <ivannissimrch@gmail.com>
2024-04-13 13:47:31 -0400

    Add MDXRemote to display selected blog post

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -1,23 +1,21 @@
-import React from 'react';
+import React from "react";
+import BlogHero from "@/components/BlogHero";
+import styles from "./postSlug.module.css";
+import { MDXRemote } from "next-mdx-remote/rsc";
+import { loadBlogPost } from "@/helpers/file-helpers";
 
-import BlogHero from '@/components/BlogHero';
+async function BlogPost({ params }) {
+  const post = await loadBlogPost(params.postSlug);
+  const {
+    content,
+    frontmatter: { title, publishedOn },
+  } = post;
 
-import styles from './postSlug.module.css';
-
-function BlogPost() {
   return (
     <article className={styles.wrapper}>
-      <BlogHero
-        title="Example post!"
-        publishedOn={new Date()}
-      />
+      <BlogHero title={title} publishedOn={new Date(publishedOn)} />
       <div className={styles.page}>
-        <p>This is where the blog post will go!</p>
-        <p>
-          You will need to use <em>MDX</em> to render all of
-          the elements created from the blog post in this
-          spot.
-        </p>
+        <MDXRemote source={content} />
       </div>
     </article>
   );
