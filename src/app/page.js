3a3b93dfbd62020d6ed8858bc455a7165ecd3ef5
ivannissimrch <ivannissimrch@gmail.com>
2024-04-13 11:19:06 -0400

    Add blogPosts to homepage, add id to use as unique keys

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,29 @@
-import React from 'react';
+import React from "react";
+import { v4 as uuidv4 } from "uuid";
 
-import BlogSummaryCard from '@/components/BlogSummaryCard';
+import BlogSummaryCard from "@/components/BlogSummaryCard";
 
-import styles from './homepage.module.css';
+import styles from "./homepage.module.css";
+import { getBlogPostList } from "@/helpers/file-helpers";
 
-function Home() {
+async function Home() {
+  const blogPosts = await getBlogPostList();
+
   return (
     <div className={styles.wrapper}>
-      <h1 className={styles.mainHeading}>
-        Latest Content:
-      </h1>
-
-      {/* TODO: Iterate over the data read from the file system! */}
-      <BlogSummaryCard
-        slug="example"
-        title="Hello world!"
-        abstract="This is a placeholder, an example which shows how the “BlogSummaryCard” component should be used. You'll want to swap this out based on the data from the various MDX files!"
-        publishedOn={new Date()}
-      />
+      <h1 className={styles.mainHeading}>Latest Content:</h1>
+      {blogPosts.map((blog) => {
+        const { slug, title, abstract } = blog;
+        return (
+          <BlogSummaryCard
+            key={uuidv4()}
+            slug={slug}
+            title={title}
+            abstract={abstract}
+            publishedOn={new Date()}
+          />
+        );
+      })}
     </div>
   );
 }
