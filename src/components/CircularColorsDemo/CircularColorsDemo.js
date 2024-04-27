3542297d5e21d6beca47a4aca481e7ee9e16f691
ivannissimrch <ivannissimrch@gmail.com>
2024-04-27 07:35:49 -0400

    Remove unused useEffect import from CircularColorsDemo component

--- src/components/CircularColorsDemo/CircularColorsDemo.js
+++ src/components/CircularColorsDemo/CircularColorsDemo.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import clsx from "clsx";
 import { Play, Pause, RotateCcw } from "react-feather";
 
